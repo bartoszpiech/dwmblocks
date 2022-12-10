e9e741377117a7f675533d30e120b5c8b1669e4f
barti <barti@x200.my.domain>
2022-12-10 11:48:49 +0100

    config update

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
-#define CMDLENGTH 45
-#define DELIMITER "|"
+#define CMDLENGTH 128
+#define DELIMITER "    "
 #define CLICKABLE_BLOCKS
+
+#define STATUSBAR_SCRPTS_DIR	"/home/barti/.local/bin/statusbar/"
 
 const Block blocks[] = {
 	//BLOCK("sb-mail",    1800, 17),
@@ -11,7 +13,9 @@ const Block blocks[] = {
 	//BLOCK("sb-mic",     0,    26),
 	//BLOCK("sb-record",  0,    27),
 	//BLOCK("sb-volume",  0,    22),
-	BLOCK("/home/barti/.local/bin/statusbar/sb_clock", 60,    23),
-	BLOCK("/home/barti/.local/bin/statusbar/sb_battery", 5,    24),
-	//BLOCK("/sb_clock",    1,    24)
+	 BLOCK("/home/barti/.local/bin/statusbar/sb_cpu",		5,		0),
+	 BLOCK("/home/barti/.local/bin/statusbar/sb_memory",	5,		0),
+	 BLOCK("/home/barti/.local/bin/statusbar/sb_internet",	60,		0),
+	 BLOCK("/home/barti/.local/bin/statusbar/sb_battery",	60,		0),
+	 BLOCK("/home/barti/.local/bin/statusbar/sb_clock",		60,		0),
 };
